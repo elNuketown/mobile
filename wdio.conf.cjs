454fdd2f262a7379e1acff1a290d3d52e44b3ebb
Vinicios Virissimo <viniciosvirissimo@hotmail.com>
2025-06-23 00:52:15 -0300

    teste

--- wdio.conf.cjs
+++ wdio.conf.cjs
@@ -1,31 +1,21 @@
 exports.config = {
-  runner: 'local',
   path: '/wd/hub',
+  user: process.env.BROWSERSTACK_USERNAME,
+  key:  process.env.BROWSERSTACK_ACCESS_KEY,
   specs: ['./tests/**/*.spec.js'],
   maxInstances: 1,
-
-  user: process.env.BROWSERSTACK_USERNAME,
-  key: process.env.BROWSERSTACK_ACCESS_KEY,
-
   capabilities: [{
-    'platformName': 'Android',
+    platformName: 'Android',
     'appium:deviceName': 'Google Pixel 7',
-    'appium:platformVersion': '13.0',
-    'appium:app': 'bs://b7ee0761bb5fc977050358540241ea89c5147553',
-    'appium:automationName': 'UiAutomator2'
+    'appium:automationName': 'UiAutomator2',
+    'appium:app': 'bs://b7ee0761bb5fc977050358540241ea89c5147553'
   }],
-
   logLevel: 'info',
-  waitforTimeout: 10000,
   framework: 'mocha',
-  mochaOpts: {
-    timeout: 60000
-  },
   reporters: ['spec', ['mochawesome', {
     outputDir: './reports',
     reportFilename: 'index',
-    quiet: true,
-    html: true,
-    json: false,
+    html: true
   }]],
+  mochaOpts: { timeout: 60000 }
 };
