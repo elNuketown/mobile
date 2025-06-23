6fb02f3fe95a6c2d5b443e01d103717a6481c1c9
Vinicios Virissimo <viniciosvirissimo@hotmail.com>
2025-06-23 13:24:31 -0300

    ajustes

--- utils/login.js
+++
@@ -1,7 +0,0 @@
-async function login(driver, user = 'standard_user', pass = 'secret_sauce') {
-  await driver.$('~test-Username').setValue(user);
-  await driver.$('~test-Password').setValue(pass);
-  await driver.$('~test-LOGIN').click();
-}
-
-module.exports = { login };
